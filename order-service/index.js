1d003af360bc6fbe0b76f982d23a383f79c5246f
augmented <augmented@augmenteds-MBP.lan>
2025-08-10 15:35:54 +0530

    kube

--- order-service/index.js
+++ order-service/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const axios = require("axios");
 
 const orders = [];
 
 function checkUserDataHeader(req, res, next) {
-  const userData = req.headers['x-user-data'];
+  const userData = req.headers["x-user-data"];
 
   if (!userData) {
     return res.status(400).json({ message: "Missing 'x-user-data' header" });
@@ -14,15 +15,29 @@ function checkUserDataHeader(req, res, next) {
   try {
     req.userData = JSON.parse(userData);
   } catch (err) {
-    return res.status(400).json({ message: "'x-user-data' header must be valid JSON" });
+    return res
+      .status(400)
+      .json({ message: "'x-user-data' header must be valid JSON" });
   }
 
   next();
 }
 
+async function getProductInfo(productId, userData) {
+  const response = await axios.get(
+    `http://product-service.product.svc.cluster.local:4005/product/${productId}`,
+    {
+      headers: {
+        "x-user-data": JSON.stringify(userData),
+      },
+    }
+  );
+  return response.data;
+}
+
 app.post("/orders", checkUserDataHeader, (req, res) => {
   const { productId } = req.body;
-   const { userId }= req.userData
+  const userId = req.userData.id;
   if (!userId || !productId) {
     return res.status(400).json({ message: "userId and productId required" });
   }
@@ -31,8 +46,14 @@ app.post("/orders", checkUserDataHeader, (req, res) => {
   res.status(201).json(order);
 });
 
-app.get("/orders", checkUserDataHeader, (req, res) => {
-  res.json(orders);
+app.get("/orders", checkUserDataHeader, async (req, res) => {
+  const ordersWithProductInfo = await Promise.all(
+    orders.map(async (order) => {
+      const productInfo = await getProductInfo(order.productId, req.userData);
+      return { ...order, product: {...productInfo }};
+    })
+  );
+  res.json(ordersWithProductInfo);
 });
 
 app.listen(4003, () => console.log("Order Service running on port 4003"));
--- product-service/index.js
+++ product-service/index.js
@@ -4,11 +4,11 @@ const app = express();
 const products = [
   { id: 1, name: "Laptop", price: 1000 },
   { id: 2, name: "Phone", price: 500 },
-  { id: 3, name: "Headphones", price: 100 }
+  { id: 3, name: "Headphones", price: 100 },
 ];
 
 function checkUserDataHeader(req, res, next) {
-  const userData = req.headers['x-user-data'];
+  const userData = req.headers["x-user-data"];
 
   if (!userData) {
     return res.status(400).json({ message: "Missing 'x-user-data' header" });
@@ -17,11 +17,17 @@ function checkUserDataHeader(req, res, next) {
   try {
     req.userData = JSON.parse(userData);
   } catch (err) {
-    return res.status(400).json({ message: "'x-user-data' header must be valid JSON" });
+    return res
+      .status(400)
+      .json({ message: "'x-user-data' header must be valid JSON" });
   }
 
   next();
 }
+
+app.get("/product/:productId", checkUserDataHeader, (req, res) => {
+  res.json(products.find((data) => data.id === Number(req.params.productId)));
+});
 
 app.get("/products", checkUserDataHeader, (req, res) => {
   res.json(products);
